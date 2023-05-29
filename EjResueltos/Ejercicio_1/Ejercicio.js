db3f8a4b48d175abb45235906a37d0ae7233c77b
klismannSis <kchancuana@unsa.edu.pe>
2023-05-28 19:57:21 -0500

    //COMPLETO/FUNCIONABLE.

--- EjResueltos/Ejercicio_1/Ejercicio.js
+++ EjResueltos/Ejercicio_1/Ejercicio.js
@@ -1,29 +1,12 @@
-function recitar(markupText) {
-const url = 'http://localhost:3000/'
-const data = {
-	text: markupText
-}
-console.log(data)
-const request = {
-	method: 'POST', // Podría ser GET
-	headers: {
-		'Content-Type': 'application/json',
-	},
-	body: JSON.stringify(data),
-}
-http = fetch(url, request)
-http.then(
-	response => response.json()
-).then(
-	data => {
-		document.querySelector("#htmlCode").innerHTML = data.text
-	}
-    )
-}
-document.addEventListener('DOMContentLoaded', function () {
-    const text = document.querySelector('#markupText')
-    document.querySelector('#markupForm').onsubmit = () => {
-        recitar(text.value)
-        return false;
-    }
-})
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Escuchando en: http://localhost:3000")
+
+});
+
+app.get('/', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'index.html'));
+});
